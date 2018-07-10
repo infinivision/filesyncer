b1a90d286f75e154f8f6c020e714fbfc4f9ffae7
Zhichang Yu <yuzhichang@gmail.com>
2018-07-10 10:33:14 +0800

    updated build.sh

--- build.sh
+++ build.sh
@@ -1,12 +1,19 @@
 #!/usr/bin/env bash
 
-DIRS="cmd/faceclient cmd/faceserver cmd/server"
+DIRS="cmd/faceserver cmd/server"
+DIRS_ARM="cmd/faceclient"
 PKG_VERSION="github.com/infinivision/filesyncer/pkg/version"
 
 GIT_SHA=$(git rev-parse --short HEAD || echo "GitNotFound")
 BUILD_TIME=$(date --iso-8601=seconds)
 for dir in ${DIRS}; do
-	pushd ${dir}
+    pushd ${dir}
     go build -ldflags "-X ${PKG_VERSION}.GitSHA=${GIT_SHA} -X ${PKG_VERSION}.BuildTime=${BUILD_TIME}"
-  	popd
+    popd
+done
+
+for dir in ${DIRS_ARM}; do
+    pushd ${dir}
+    GOOS=linux GOARCH=arm64 GOARM=7 go build -ldflags "-X ${PKG_VERSION}.GitSHA=${GIT_SHA} -X ${PKG_VERSION}.BuildTime=${BUILD_TIME}"
+    popd
 done
