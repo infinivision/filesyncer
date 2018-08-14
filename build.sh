067acfbc5597a14d822c1d8b85fef51729832b49
Zhichang Yu <yuzhichang@gmail.com>
2018-08-14 21:07:18 +0800

    changed build.sh

--- build.sh
+++ build.sh
@@ -8,12 +8,12 @@ GIT_SHA=$(git rev-parse --short HEAD || echo "GitNotFound")
 BUILD_TIME=$(date --iso-8601=seconds)
 for dir in ${DIRS}; do
     pushd ${dir}
-    go build -ldflags "-X ${PKG_VERSION}.GitSHA=${GIT_SHA} -X ${PKG_VERSION}.BuildTime=${BUILD_TIME}"
+    GO111MODULE=on go build -ldflags "-X ${PKG_VERSION}.GitSHA=${GIT_SHA} -X ${PKG_VERSION}.BuildTime=${BUILD_TIME}"
     popd
 done
 
 for dir in ${DIRS_ARM}; do
     pushd ${dir}
-    GOOS=linux GOARCH=arm64 GOARM=7 go build -ldflags "-X ${PKG_VERSION}.GitSHA=${GIT_SHA} -X ${PKG_VERSION}.BuildTime=${BUILD_TIME}"
+    GO111MODULE=on GOOS=linux GOARCH=arm64 GOARM=7 go build -ldflags "-X ${PKG_VERSION}.GitSHA=${GIT_SHA} -X ${PKG_VERSION}.BuildTime=${BUILD_TIME}"
     popd
 done
