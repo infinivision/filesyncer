78a600ffa6d0fc8e3ea5cb85204a02ec159e13c1
Zhichang Yu <yuzhichang@gmail.com>
2018-12-29 16:21:47 +0800

    more log

--- cmd/faceserver/identifier3.go
+++ cmd/faceserver/identifier3.go
@@ -227,7 +227,7 @@ func (this *Identifier3) Identify(vecMsg VecMsg) (visit *Visit, err error) {
 	}
 	if cnt1 != 0 || cnt2 != 0 {
 		t0 = time.Now()
-		log.Infof("hyena added xids %+v", newXids)
+		log.Infof("hyena added xids %+v, %016x", newXids, uint64(newXids[0]))
 		if err = this.vdb.AddWithIds(vecMsg.Vec, newXids); err != nil {
 			err = errors.Wrap(err, "")
 			return
