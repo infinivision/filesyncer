9d16f10b099e30bfa4544aefc0023341a928964f
Zhichang Yu <yuzhichang@gmail.com>
2019-04-18 14:22:59 +0800

    filter images by uid

--- cmd/fetchImgs/fetchImgs.go
+++ cmd/fetchImgs/fetchImgs.go
@@ -6,6 +6,8 @@ import (
 	"io/ioutil"
 	"os"
 	"os/user"
+	"strconv"
+	"strings"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/credentials"
@@ -26,8 +28,8 @@ var (
 	ossBucket    = flag.String("oss-bucket", "images", "oss bucket name")
 	dateStart    = flag.String("date-start", "", "Datatime: date start in RFC3339 format. For example: 2019-03-01T00:00:00+08:00")
 	dateEnd      = flag.String("date-end", "", "Datatime: date end in RFC3339 format")
-
-	output = flag.String("output", "", "output directory")
+	uids         = flag.String("uids", "", "interested user ids. Empyt means all. For example: 1226,3495")
+	output       = flag.String("output", "", "output directory")
 )
 
 func s3Get(srv *s3.S3, key string) (value []byte, err error) {
@@ -48,6 +50,7 @@ func s3Get(srv *s3.S3, key string) (value []byte, err error) {
 }
 
 func main() {
+	var err error
 	flag.Parse()
 	if *output == "" {
 		usr, err := user.Current()
@@ -56,8 +59,19 @@ func main() {
 		}
 		*output = usr.HomeDir
 	}
+	intUids := make(map[uint64]int)
+	var intUid uint64
+	for _, sUid := range strings.Split(*uids, ",") {
+		if sUid == "" {
+			continue
+		}
+		if intUid, err = strconv.ParseUint(sUid, 10, 64); err != nil {
+			err = errors.Errorf("invalid uid: %v", sUid)
+			log.Fatal(err)
+		}
+		intUids[intUid] = 0
+	}
 
-	var err error
 	var sess *session.Session
 	sess = session.Must(session.NewSession(&aws.Config{
 		Credentials:      credentials.NewStaticCredentials(*ossKey, *ossSecretKey, ""),
@@ -80,43 +94,54 @@ func main() {
 		log.Fatal(err)
 	}
 
-	var recs []string
-	if recs, err = rcli.LRange(que, int64(idxStart), int64(idxEnd-1)).Result(); err != nil {
-		err = errors.Wrapf(err, "")
-		log.Fatal(err)
-	}
-	for _, rec := range recs {
-		var visit server.Visit
-		if err = visit.Unmarshal([]byte(rec)); err != nil {
+	numFetched := 0
+	batchSize := int64(100)
+	for idxCur := idxStart; idxCur < idxEnd; idxCur += batchSize {
+		var recs []string
+		if recs, err = rcli.LRange(que, idxCur, idxCur+batchSize-1).Result(); err != nil {
 			err = errors.Wrapf(err, "")
 			log.Fatal(err)
 		}
+		for _, rec := range recs {
+			var visit server.Visit
+			if err = visit.Unmarshal([]byte(rec)); err != nil {
+				err = errors.Wrapf(err, "")
+				log.Fatal(err)
+			}
+			if len(intUids) != 0 {
+				if _, found := intUids[visit.Uid]; !found {
+					continue
+				}
+			}
+			numFetched += 1
 
-		objID := visit.PictureId
-		uid := visit.Uid
-		age := visit.Age
-		gender := visit.Gender
-		fp := fmt.Sprintf("%v/%v_%v_%v_%v.jpg", *output, uid, gender, age, objID)
-		if _, err = os.Stat(fp); err == nil {
-			log.Infof("%v already exist", fp)
-			continue
+			uid := visit.Uid
+			visitTime := visit.VisitTime
+			gender := visit.Gender
+			age := visit.Age
+			objID := visit.PictureId
+			fp := fmt.Sprintf("%v/%d_%d_%d_%d_%v.jpg", *output, uid, visitTime, gender, age, objID)
+			if _, err = os.Stat(fp); err == nil {
+				log.Infof("%v already exist", fp)
+				continue
+			}
+			var img []byte
+			if img, err = s3Get(srv, objID); err != nil {
+				log.Fatalf("got error %+v", err)
+			}
+			var jpg *os.File
+			if jpg, err = os.OpenFile(fp, os.O_TRUNC|os.O_CREATE|os.O_RDWR, 0755); err != nil {
+				err = errors.Wrap(err, "")
+				log.Fatalf("got error %+v", err)
+			}
+			if _, err = jpg.Write(img); err != nil {
+				err = errors.Wrap(err, "")
+				log.Fatalf("got error %+v", err)
+			}
+			jpg.Close()
 		}
-		var img []byte
-		if img, err = s3Get(srv, objID); err != nil {
-			log.Fatalf("got error %+v", err)
-		}
-		var jpg *os.File
-		if jpg, err = os.OpenFile(fp, os.O_TRUNC|os.O_CREATE|os.O_RDWR, 0755); err != nil {
-			err = errors.Wrap(err, "")
-			log.Fatalf("got error %+v", err)
-		}
-		if _, err = jpg.Write(img); err != nil {
-			err = errors.Wrap(err, "")
-			log.Fatalf("got error %+v", err)
-		}
-		jpg.Close()
 	}
 
-	log.Infof("Fetched %v pictures", len(recs))
+	log.Infof("Fetched %v pictures", numFetched)
 	return
 }
